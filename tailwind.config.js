53b2da1b09095a88ae2b5216053f850eedb5b3fc
Lakpriya <denuwanlakpriya44@gmail.com>
2026-05-07 01:50:52 +0530

    Initialize Vue 3 + Vite + TypeScript + Tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {fontFamily: {
+        display: ['Playfair Display', 'serif'],
+        sans: ['DM Sans', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          50: '#fff7ed',
+          100: '#ffedd5',
+          500: '#f97316',
+          600: '#ea580c',
+          700: '#c2410c',
+        }
+      }},
+  },
+  plugins: [],
+
+}
