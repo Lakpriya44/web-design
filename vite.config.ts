e82f1ef124575a35f907957c11b4d7a3cedd4d51
Lakpriya <denuwanlakpriya44@gmail.com>
2026-07-23 01:42:54 +0530

    Added mobile access functionality

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true, // or host: '0.0.0.0'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
